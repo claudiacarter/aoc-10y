ceccd1e84a9ffcdd3e3e5809f4dcb705f096a401
Claudia Carter <cloudycarter@gmail.com>
2025-07-06 12:24:50 +0100

    replaced old logic with steps for coords, incl part 1 answer

--- day1/2016/block_distance.py
+++ day1/2016/block_distance.py
@@ -36,15 +36,11 @@ def get_coords_for_move(start_coord, num_blocks, direction):
         exit(1)
     return coords
 
-instructions = parse_instructions("/Users/cc52/repositories/personal/aoc-10y/day1/2016/test_input.txt")
+instructions = parse_instructions("/Users/cc52/repositories/personal/aoc-10y/day1/2016/input.txt")
 directions = ["N","E","S","W"]
 
 # Set up direction trackers
 current_direction = directions[0]
-steps_north = 0
-steps_east = 0
-steps_south = 0
-steps_west = 0
 
 # Initiate coordinates history
 current_coord = (0,0)
@@ -79,16 +75,15 @@ for instruction in instructions:
     coord_hist.extend(new_coords)
     current_coord = new_coords[-1]
 
-y_blocks = steps_north - steps_south
-x_blocks = steps_east - steps_west
-total_blocks = abs(x_blocks) + abs(y_blocks)
+# Part 1 Answer
+last_coord = coord_hist[-1]
+min_blocks = abs(last_coord[0]) + abs(last_coord[1])
+print("\nAnswer to Part 1")
+print(f"Last coord: {last_coord}")
+print(f"Minimum blocks away: {min_blocks}")
 
+# Part 2 Answer
 first_return_coords = return_coord[0]
 first_return_blocks = abs(first_return_coords[0]) + abs(first_return_coords[1])
-
-print(return_coord)
-print(len(coord_hist))
-print(len(instructions))
-print(f"Steps North: {steps_north}, Steps East: {steps_east}, Steps South: {steps_south}, Steps West: {steps_west}")
-print(f"Minimum number of blocks away: {total_blocks}")
-print(f"First returned to {first_return_coords}, {first_return_blocks} blocks away from origin.")
+print("\nAnswer to Part 2")
+print(f"First returned to coord {first_return_coords}, {first_return_blocks} blocks away")
