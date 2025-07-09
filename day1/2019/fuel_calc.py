d7de4da5ebf65ce97ee5f8212f3818d05fde360b
Claudia Carter <cloudycarter@gmail.com>
2025-07-09 22:33:08 +0100

    soln part two fuel for fuel calculations

--- day1/2019/fuel_calc.py
+++ day1/2019/fuel_calc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import math
-
+from typing import List
 '''
 Every line is the mass of a module
 To each line you want to divide by 3, round down and minus 2
@@ -11,13 +11,52 @@ def get_input(text_file):
         input = file.read().splitlines()
     return input
 
-module_masses = get_input("input.txt")
-fuel_calcs = []
-
-for mass in module_masses:
+def convert_mass_to_fuel(mass):
     tmp = int(mass) // 3
     fuel = tmp - 2
-    fuel_calcs.append(fuel)
+    return fuel
 
-answer_1 = sum(fuel_calcs)
-print(f"Total fuel required: {answer_1}")
+
+module_masses = get_input("input.txt")
+module_fuel = [convert_mass_to_fuel(mass) for mass in module_masses]
+
+answer_1 = sum(module_fuel)
+print(f"Total fuel required for modules: {answer_1}")
+
+total_fuel = 0
+for mass in module_masses:
+    fuel = convert_mass_to_fuel(mass)
+    total_fuel += fuel
+    while convert_mass_to_fuel(fuel) > 0:
+        total_fuel += convert_mass_to_fuel(fuel)
+        fuel = convert_mass_to_fuel(fuel)
+
+# Answer 2 - includes fuel for fuel mass
+print(f"Total fuel required for launch: {total_fuel}")
+
+    
+    
+
+
+# #Initiate a list for fuel for fuel
+# fuel_fuel_calcs = []
+
+# # Start a conditional for fuel being negligible
+# neg_fuel = False
+# unfuelled_mass = module_fuel_calcs
+
+# while not neg_fuel:
+#     unfuelled_mass = convert_mass_to_fuel(unfuelled_mass)
+#     if unfuelled_mass < 0:
+#         neg_fuel == True
+#         break
+#     else:
+#         fuel_fuel_calcs.append(unfuelled_mass)
+    
+# print(unfuelled_mass)
+
+
+
+
+
+
